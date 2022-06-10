17cc2a49321a17d5a70ff72e5d136124b9045b05
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-06-10 07:01:46 -0300

    Finalizando quase todos os requisitos obrigatórios, só falta ver o que aconteceu com o requisito 4 e finalizar o 18

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const button = document.getElementById('btn');
 
-function login() {
+button.addEventListener('click', () => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const eml = 'tryber@teste.com';
   const psswrd = '123456';
-  console.log(email);
-  console.log('clicado');
   if (email === eml && password === psswrd) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-}
-
-button.addEventListener('click', login());
+});
